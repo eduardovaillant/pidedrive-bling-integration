9c4565c6ebb63e517126d59fef90223bbcff3eba
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-07-16 19:48:18 -0300

    refactor: rename variable

--- src/infra/db/mongo-helper.ts
+++ src/infra/db/mongo-helper.ts
@@ -3,11 +3,11 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
-  uri: null as string,
+  url: null as string,
 
-  async connect (uri: string): Promise<void> {
-    this.uri = uri
-    this.client = await MongoClient.connect(uri)
+  async connect (url: string): Promise<void> {
+    this.url = url
+    this.client = await MongoClient.connect(url)
   },
 
   async disconnect (): Promise<void> {
@@ -17,7 +17,7 @@ export const MongoHelper = {
 
   async getCollection (name: string): Promise<Collection> {
     if (!this.client?.isConnected) {
-      await this.connect(this.uri)
+      await this.connect(this.url)
     }
     return this.client.db().collection(name)
   },
