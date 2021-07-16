b67b8dcfa83bb57b5aedbc1071b0e61180026c3e
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-07-16 19:25:40 -0300

    feat: add server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,5 +5,6 @@ dotenv.config()
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/api',
   pipedriveBaseURL: process.env.PIPEDRIVE_BASE_URL || 'https://api.pipedrive.com/v1/',
-  pipedriveToken: process.env.PIPEDRIVE_TOKEN || ''
+  pipedriveToken: process.env.PIPEDRIVE_TOKEN || '',
+  port: process.env.PORT || 3000
 }
