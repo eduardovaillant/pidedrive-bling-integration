25bde4a3554d4d826a4ef796ab319b2d54c0f962
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-07-17 09:48:04 -0300

    feat: add new variables on env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,6 +5,8 @@ dotenv.config()
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/api',
   pipedriveBaseURL: process.env.PIPEDRIVE_BASE_URL || 'https://api.pipedrive.com/v1/',
-  pipedriveToken: process.env.PIPEDRIVE_TOKEN || '27ca3408e6f2fbecd49138f0413bc3b57617d060',
+  pipedriveToken: process.env.PIPEDRIVE_TOKEN || '',
+  blingBaseUrl: process.env.BLING_BASE_URL || 'https://bling.com.br/Api/v2/',
+  blingToken: process.env.BLING_TOKEN || '',
   port: process.env.PORT || 3000
 }
